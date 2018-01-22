48d45fdf4741469398cd2f13d884bc4841dbb368
daften <dieterblomme@gmail.com>
2018-01-22 11:39:23 +0100

    Fixed cleandirLimit as variable

--- src/AbstractRoboFile.php
+++ src/AbstractRoboFile.php
@@ -1037,8 +1037,8 @@ abstract class AbstractRoboFile extends \Robo\Tasks implements DigipolisProperti
         return $this->taskSsh($worker, $auth)
                 ->remoteDirectory($currentProjectRoot, true)
                 ->timeout(30)
-                ->exec('vendor/bin/robo digipolis:clean-dir ' . $remote['releasesdir'] . (!empty(cleandirLimit) ? ':' . $cleandirLimit : ''))
-                ->exec('vendor/bin/robo digipolis:clean-dir ' . $remote['backupsdir'] . (!empty(cleandirLimit) ? ':' . $cleandirLimit-1 : ''));
+                ->exec('vendor/bin/robo digipolis:clean-dir ' . $remote['releasesdir'] . (!empty($cleandirLimit) ? ':' . $cleandirLimit : ''))
+                ->exec('vendor/bin/robo digipolis:clean-dir ' . $remote['backupsdir'] . (!empty($cleandirLimit) ? ':' . $cleandirLimit-1 : ''));
     }
 
     /**
