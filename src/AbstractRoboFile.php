a73e44e756b37e2fe1a47fe325fcc32a675131eb
Jelle Sebreghts <sebreghts.jelle@gmail.com>
2018-09-21 11:48:27 +0200

    Fixes #23: `readlink -e` option not recognized on MacOS.

--- src/AbstractRoboFile.php
+++ src/AbstractRoboFile.php
@@ -520,7 +520,7 @@ abstract class AbstractRoboFile extends \Robo\Tasks implements DigipolisProperti
 
             $collection->addTask(
                 $this->taskRsync()
-                    ->rawArg('--rsh "ssh -o StrictHostKeyChecking=no -i `readlink -e ' . $sourceKeyFile . '`"')
+                    ->rawArg('--rsh "ssh -o StrictHostKeyChecking=no -i ' . $this->realpath($sourceKeyFile) . '"')
                     ->fromPath($tmpKeyFile)
                     ->toHost($sourceHost)
                     ->toUser($sourceUser)
@@ -542,7 +542,7 @@ abstract class AbstractRoboFile extends \Robo\Tasks implements DigipolisProperti
                 $dir .= ($dir !== '' ? '/' : '');
 
                 $rsync = $this->taskRsync()
-                    ->rawArg('--rsh "ssh -o StrictHostKeyChecking=no -i `readlink -e ' . $tmpKeyFile . '`"')
+                    ->rawArg('--rsh "ssh -o StrictHostKeyChecking=no -i ' . $this->realpath($tmpKeyFile) . '"')
                     ->fromPath($sourceRemote['filesdir'] . '/' . $dir)
                     ->toHost($destinationHost)
                     ->toUser($destinationUser)
@@ -1125,7 +1125,7 @@ abstract class AbstractRoboFile extends \Robo\Tasks implements DigipolisProperti
                     $dir .= ($dir !== '' ? '/' : '');
 
                     $rsync = $this->taskRsync()
-                        ->rawArg('--rsh "ssh -o StrictHostKeyChecking=no -i `readlink -e ' . $keyFile . '`"')
+                        ->rawArg('--rsh "ssh -o StrictHostKeyChecking=no -i ' . $this->realpath($keyFile) . '"')
                         ->fromHost($host)
                         ->fromUser($user)
                         ->fromPath($remote['filesdir'] . '/' . $dir)
@@ -1313,5 +1313,39 @@ abstract class AbstractRoboFile extends \Robo\Tasks implements DigipolisProperti
             '[time]' => is_null($timestamp) ? $this->time : $timestamp,
         );
         return $this->tokenReplace($this->getConfig()->get('local'), $replacements) + $defaults;
+    }
+
+    /**
+     * PHP's realpath can't handle tilde (~), so we have to write a wrapper
+     * for it.
+     */
+    protected function realpath($path)
+    {
+        if (strpos($path, '~') === 0 && ($homedir = $this->getUserHomeDir())) {
+            $path = $homedir . substr($path, 1);
+        }
+        return realpath($path);
+    }
+
+    /**
+     * Get the home directory for the current user.
+     */
+    protected function getUserHomeDir()
+    {
+        // getenv('HOME') isn't set on Windows.
+        $home = getenv('HOME');
+        if (!empty($home)) {
+            // home should never end with a trailing slash.
+            return rtrim($home, '/');
+        }
+        if (!empty($_SERVER['HOMEDRIVE']) && !empty($_SERVER['HOMEPATH'])) {
+            // home on windows
+            $home = $_SERVER['HOMEDRIVE'] . $_SERVER['HOMEPATH'];
+            // If HOMEPATH is a root directory the path can end with a slash.
+            // Make sure that doesn't happen.
+            return rtrim($home, '\\/');
+        }
+
+        return false;
     }
 }
